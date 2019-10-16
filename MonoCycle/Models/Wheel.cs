caf206de424ac842625d19038d231c9f2b602f76
Сергій Лакас <serhiy.lakas@hotmail.com>
2019-10-16 19:38:20 +0300

    Add class wheel

---
+++ MonoCycle/Models/Wheel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonoCycle.Models
+{
+    public sealed class Wheel : IDisposable
+    {
+        public double Mass { get; set; } = 1;
+        public double Power { get; set; }
+        public double WheelLength => wheelLength;
+        public Direction Direction { get; set; }
+
+        double wheelLength;
+
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
